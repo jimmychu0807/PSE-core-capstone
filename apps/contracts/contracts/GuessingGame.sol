51fe92ddfdccbe04f1dc7a5257a6961a543ca0c6
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-08-14 23:09:35 +0800

    Added submit bid function (#4)
    * updated
    
    * Can submit bids

--- apps/contracts/contracts/GuessingGame.sol
+++ apps/contracts/contracts/GuessingGame.sol
@@ -30,10 +30,33 @@ contract GuessingGame is IGuessingGame, Ownable {
     _;
   }
 
-  modifier gameStateEq(uint32 gameId, GameState gameState) {
+  modifier gameStateIn2(uint32 gameId, GameState[2] memory gss) {
     Game storage game = games[gameId];
-    if (game.state != gameState) {
-      revert GuessingGame__UnexpectedGameState(gameState, game.state);
+    if (game.state != gss[0] && game.state != gss[1]) {
+      revert GuessingGame__UnexpectedGameState(game.state);
+    }
+    _;
+  }
+
+  modifier oneOfPlayers(uint32 gameId) {
+    Game storage game = games[gameId];
+    bool found = false;
+    for (uint8 i = 0; i < game.players.length; i++) {
+      if (game.players[i] == msg.sender) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      revert GuessingGame__SenderNotOneOfPlayers();
+    }
+    _;
+  }
+
+  modifier gameStateEq(uint32 gameId, GameState gs) {
+    Game storage game = games[gameId];
+    if (game.state != gs) {
+      revert GuessingGame__UnexpectedGameState(game.state);
     }
     _;
   }
@@ -59,6 +82,8 @@ contract GuessingGame is IGuessingGame, Ownable {
     } else if (state == GameState.GameEnd) {
       game.endTime = game.lastUpdate;
     }
+
+    emit GameStateUpdated(gameId, state);
   }
 
   function newGame() external override returns (uint32 gameId) {
@@ -70,6 +95,8 @@ contract GuessingGame is IGuessingGame, Ownable {
     emit NewGame(gameId, msg.sender);
   }
 
+  // IMPROVE: the gameStateIn() modifier code is bad. It is restricted to take
+  //   two params.
   function joinGame(uint32 gameId) external override validGameId(gameId) gameStateEq(gameId, GameState.GameInitiated) {
     Game storage game = games[gameId];
     // check the player has not been added to the game
@@ -83,10 +110,44 @@ contract GuessingGame is IGuessingGame, Ownable {
     emit PlayerJoinGame(gameId, msg.sender);
   }
 
-  function startGame(
+  function startRound(
     uint32 gameId
-  ) external override validGameId(gameId) byGameHost(gameId) gameStateEq(gameId, GameState.GameInitiated) {
-    _updateGameState(gameId, GameState.RoundRunningBid);
+  ) external override validGameId(gameId) byGameHost(gameId) gameStateIn2(gameId, [GameState.GameInitiated, GameState.RoundEnd]) {
+    _updateGameState(gameId, GameState.RoundBid);
     emit GameStarted(gameId);
+  }
+
+  function submitBid(
+    uint32 gameId,
+    bytes32 bid_null_hash,
+    bytes32 null_hash
+  ) external override validGameId(gameId) oneOfPlayers(gameId) gameStateEq(gameId, GameState.RoundBid) {
+    // each player submit a bid. The last player that submit a bid will change the game state
+    Game storage game = games[gameId];
+    uint8 round = game.currentRound;
+    game.bids[round][msg.sender] = Bid(bid_null_hash, null_hash);
+    emit BidSubmitted(gameId, round, msg.sender);
+
+    // If all players have submitted bid, update game state
+    bool notYetBid = false;
+    for (uint i = 0; i < game.players.length; i++) {
+      address p = game.players[i];
+      if (game.bids[round][p].bid_null_hash == bytes32(0)) {
+        notYetBid = true;
+        break;
+      }
+    }
+
+    if (!notYetBid) {
+      _updateGameState(gameId, GameState.RoundReveal);
+    }
+  }
+
+  function revealBid() {
+    // each player reveal a bid. The last player that reveal a bid will change the game state
+  }
+
+  function endRound() {
+    // the average will be cmoputed, the winner will be determined. Update the game state.
   }
 }
