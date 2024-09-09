c79c9e70d57cfed78917bc3f53ab3ae42e00389b
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-09-10 07:50:41 +0800

    Output a submission with nullifier now (#11)
    * Added nullifier in the submit-rangecheck
    
    * Test for the poseidon hashing
    
    * updated

--- apps/circuits/tests/submit-rangecheck.test.ts
+++ apps/circuits/tests/submit-rangecheck.test.ts
@@ -1,11 +1,17 @@
 import { WitnessTester } from "circomkit";
 import { circomkit } from "./common";
+const { randomInt } = require("node:crypto");
+
+// Check test here on how to build poseidon and use it:
+//   https://github.com/iden3/circomlib/blob/master/test/poseidoncircuit.js
+const buildPoseidon = require("circomlibjs").buildPoseidon;
 
 describe("submit-rangecheck", () => {
   let circuit: WitnessTester<
-    ["in"] // private inputs
+    ["in", "rand"] // private inputs
   >;
-
+  let poseidon;
+  let F;
   const [MIN, MAX, NBITS] = [1, 100, 7];
 
   before(async () => {
@@ -14,22 +20,33 @@ describe("submit-rangecheck", () => {
       template: "SubmissionRangeCheck",
       params: [MIN, MAX, NBITS],
     });
+
+    poseidon = await buildPoseidon();
+    F = poseidon.F;
   });
 
   it("Should pass when the value is within range", async () => {
-    let INPUT = { in: MIN };
-    const OUT = { out: 1 };
-    await circuit.expectPass(INPUT, OUT);
+    const rand = randomInt(281474976710655);
 
-    INPUT = { in: MAX };
-    await circuit.expectPass(INPUT, OUT);
+    let INPUT = { in: MIN, rand };
+
+    const nullifier = F.toObject(poseidon([rand]));
+    let submission = F.toObject(poseidon([MIN, rand]));
+    await circuit.expectPass(INPUT, { submission, nullifier });
+
+    INPUT = { in: MAX, rand };
+
+    submission = F.toObject(poseidon([MAX, rand]));
+    await circuit.expectPass(INPUT, { submission, nullifier });
   });
 
   it("Should fail when the value is out of range", async () => {
-    let INPUT = { in: MIN - 1 };
+    const rand = randomInt(281474976710655);
+
+    let INPUT = { in: MIN - 1, rand };
     await circuit.expectFail(INPUT);
 
-    INPUT = { in: MAX + 1 };
+    INPUT = { in: MAX + 1, rand };
     await circuit.expectFail(INPUT);
   });
 });
