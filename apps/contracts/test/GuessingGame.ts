27f81bc2e9d83f2dbda1948d6441c25377719bea
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-09-11 13:42:05 +0800

    Start to test revealing player bids/commitments (#16)
    * test game state to RoundReveal
    
    * updated dependencies
    
    * Can start for players to reveal
    
    * prettier:write

--- apps/contracts/test/GuessingGame.ts
+++ apps/contracts/test/GuessingGame.ts
@@ -1,5 +1,4 @@
-import * as chai from "chai";
-import chaiAsPromised from "chai-as-promised";
+import { expect } from "chai";
 import hre, { run } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 const { randomInt } = require("node:crypto");
@@ -7,9 +6,6 @@ const { randomInt } = require("node:crypto");
 import { GameState, prove, toOnChainProof, zeroPadNBytes } from "./helpers";
 // @ts-ignore: typechain folder will be generated after contracts compilation
 import { GuessingGame } from "../typechain-types";
-
-chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 // Defining circuit base paths
 const SUBMIT_RANGECHECK_CIRCUIT_BASEPATH = "./artifacts/circuits/submit-rangecheck-1-100";
@@ -41,7 +37,46 @@ describe("GuessingGame", () => {
     return { contracts, players: { host, bob, charlie, dave } };
   }
 
-  describe("L New Game", () => {
+  async function deployContractsGameRoundReveal() {
+    const { contracts, players } = await deployContractsGameStarted();
+    const { host, bob, charlie } = players;
+    const { gameContract } = contracts;
+
+    const GAME_ID = 0;
+    const rand = randomInt(281474976710655);
+
+    // generate proofs
+    const inputs = {
+      host: { in: 1, rand },
+      bob: { in: 3, rand },
+      charlie: { in: 5, rand },
+    };
+    // const fullProofs = await Promise.all(inputs.map((i) => prove(i, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH)));
+    const fullProofs = {
+      host: await prove(inputs.host, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH),
+      bob: await prove(inputs.bob, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH),
+      charlie: await prove(inputs.charlie, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH),
+    };
+
+    // Flatten the plonk proof to on-chain proof format
+    Object.values(fullProofs).map((pproof) => {
+      pproof.proof = toOnChainProof(pproof.proof);
+    });
+
+    await Promise.all([
+      gameContract.submitCommitment(GAME_ID, fullProofs.host.proof, fullProofs.host.publicSignals),
+      gameContract
+        .connect(bob)
+        .submitCommitment(GAME_ID, fullProofs.bob.proof, fullProofs.bob.publicSignals),
+      gameContract
+        .connect(charlie)
+        .submitCommitment(GAME_ID, fullProofs.charlie.proof, fullProofs.charlie.publicSignals),
+    ]);
+
+    return { contracts, players, inputs, fullProofs };
+  }
+
+  describe("L New Game (GameState.GameInitiated)", () => {
     it("should create a new game", async () => {
       const { contracts, players } = await loadFixture(deployContractsCleanSlate);
       const { gameContract } = contracts;
@@ -102,7 +137,7 @@ describe("GuessingGame", () => {
     });
   });
 
-  describe("L After a game started", () => {
+  describe("L After a game started (GameState.RoundBid)", () => {
     it("only players can submit a commitment, non-players cannot", async () => {
       const { contracts, players } = await loadFixture(deployContractsGameStarted);
       const { gameContract } = contracts;
@@ -150,20 +185,55 @@ describe("GuessingGame", () => {
         gameContract.submitCommitment(GAME_ID, onChainProof, publicSignals)
       ).to.be.revertedWithCustomError(gameContract, "GuessingGame__InvalidSubmitRangeCheckProof");
     });
+
+    it("when all players have submitted bids, the game state is updated", async () => {
+      const { contracts, players } = await loadFixture(deployContractsGameStarted);
+      const { gameContract } = contracts;
+      const { host, bob, charlie } = players;
+
+      const GAME_ID = 0;
+      const rands = [
+        randomInt(281474976710655),
+        randomInt(281474976710655),
+        randomInt(281474976710655),
+      ];
+
+      // host generates a commitment and submit on-chain
+      let input = { in: 1, rand: rands[0] };
+      let { proof, publicSignals } = await prove(input, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH);
+      await gameContract.submitCommitment(GAME_ID, toOnChainProof(proof), publicSignals);
+
+      // Bob's turn
+      input = { in: 3, rand: rands[1] };
+      ({ proof, publicSignals } = await prove(input, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH));
+      await gameContract
+        .connect(bob)
+        .submitCommitment(GAME_ID, toOnChainProof(proof), publicSignals);
+
+      // Charlie's turn
+      input = { in: 5, rand: rands[2] };
+      ({ proof, publicSignals } = await prove(input, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH));
+      await expect(
+        gameContract
+          .connect(charlie)
+          .submitCommitment(GAME_ID, toOnChainProof(proof), publicSignals)
+      )
+        .to.emit(gameContract, "GameStateUpdated")
+        .withArgs(GAME_ID, GameState.RoundReveal);
+
+      // check the game state
+      const game = await gameContract.getGame(GAME_ID);
+      expect(game.state).to.be.equal(GameState.RoundReveal);
+    });
   });
 
-  describe("L Range check: genarate proof offchain, verify proof onchain", () => {
-    it("should create a range proof and be verified", async () => {
-      const { contracts } = await loadFixture(deployContractsCleanSlate);
-      const { rcContract } = contracts;
-
-      const rand = randomInt(281474976710655);
-
-      // generate proof
-      const input = { in: 99, rand };
-      const { proof, publicSignals } = await prove(input, SUBMIT_RANGECHECK_CIRCUIT_BASEPATH);
-      const result = await rcContract.verifyProof(toOnChainProof(proof), publicSignals);
-      expect(result).to.be.true;
+  describe("L After all players submitted bids (GamteState.RoundReveal)", () => {
+    it("should allow player to reveal their commitments", async () => {
+      const { contracts, players, inputs, fullProofs } = await loadFixture(
+        deployContractsGameRoundReveal
+      );
+      const { gameContract } = contracts;
+      const { host } = players;
     });
   });
 });
