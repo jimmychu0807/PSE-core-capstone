e7eafe12716f8e1072bfe3b829a9faa0f9978707
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-09-15 16:10:17 +0800

    updated with GameCard

--- apps/web/src/components/GameCard.tsx
+++ apps/web/src/components/GameCard.tsx
@@ -1,67 +1,134 @@
+"use client";
+
+// 3rd-parties components
+import { useCallback, useState, useEffect } from "react";
+import { useConfig, useAccount, useWriteContract } from "wagmi";
+import { readContract } from "@wagmi/core";
+import { Link as NextLink } from "next/link";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Text,
+  UnorderedList,
+  ListItem,
+  CardFooter,
+  Button,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+
+// Components defined in this repo
+import { useGameContractConfig } from "@/hooks";
+import { GameState } from "@/config";
+import { formatter } from "@/utils";
+
 type GameCardProps = {
   gameId: number;
 };
 
 export default function GameCard({ gameId }: GameCardProps) {
-  return <div>{gameId}</div>;
-  // const { abi, deployedAddress } = gameArtifact;
-  // const contractCfg = useMemo(() => ({ abi, address: deployedAddress }), [abi, deployedAddress]);
+  const wagmiConfig = useConfig();
+  const router = useRouter();
+  const contractCfg = useGameContractConfig();
+  const { writeContractAsync, isPending } = useWriteContract();
+  const { address: userAccount } = useAccount();
+  const [game, setGame] = useState(undefined);
 
-  // const { address: userAddr } = useAccount();
-  // const { isPending, writeContract } = useWriteContract();
+  /**
+   * event handlers
+   **/
+  const joinGameHandler = useCallback(() => {
+    let setState = true;
+    const joinGame = async () => {
+      const res = await writeContractAsync({
+        ...contractCfg,
+        functionName: "joinGame",
+        args: [gameId],
+      });
 
-  // const userJoinedGame: boolean = game.players.includes(userAddr);
+      if (setState) {
+        console.log("res", res);
+        router.push(`/game/${gameId}`);
+      }
+    };
 
-  // const userJoinGameHandler = useCallback(() => {
-  //   writeContract({
-  //     ...contractCfg,
-  //     functionName: "joinGame",
-  //     args: [gameId],
-  //   });
-  // }, [writeContract, contractCfg, gameId]);
+    joinGame();
+    return () => {
+      setState = false;
+    };
+  }, [writeContractAsync, contractCfg, gameId, router]);
 
-  // return (
-  //   <Card w={500}>
-  //     <LinkBox>
-  //       <CardHeader>
-  //         <LinkOverlay as={NextLink} href={`/game/${gameId}`}>
-  //           <strong>Game ID: {gameId}</strong>
-  //         </LinkOverlay>
-  //       </CardHeader>
+  /**
+   * call on-chain `getGame()` on page load
+   **/
+  useEffect(() => {
+    // this is a good practice as unsubscription
+    let setState = true;
+    const getGame = async () => {
+      const res = await readContract(wagmiConfig, {
+        ...contractCfg,
+        functionName: "getGame",
+        args: [gameId],
+      });
+      setState && setGame(res);
+    };
 
-  //       <CardBody>
-  //         <Text>Players: {game.players.length}</Text>
-  //         <UnorderedList styleType="- ">
-  //           {game.players.map((p) => (
-  //             <ListItem key={`game-${gameId}-${p}`} fontSize={14}>
-  //               {p}
-  //             </ListItem>
-  //           ))}
-  //         </UnorderedList>
-  //         <Text>
-  //           State: <strong>{GameState[game.state]}</strong>
-  //         </Text>
-  //         <Text>Created: {formatter.dateTime(game.startTime)}</Text>
-  //         <Text>Last Updated: {formatter.dateTime(game.lastUpdate)}</Text>
-  //       </CardBody>
-  //     </LinkBox>
-  //     <CardFooter justifyContent="center">
-  //       {game.state === GameState.GameInitiated && (
-  //         <Button
-  //           onClick={userJoinGameHandler}
-  //           variant="outline"
-  //           colorScheme="blue"
-  //           isDisabled={userJoinedGame}
-  //           isLoading={isPending}
-  //         >
-  //           {userJoinedGame ? "Already Joined" : "Join Game"}
-  //         </Button>
-  //       )}
-  //       {game.state >= GameState.RoundCommit && game.state <= GameState.RoundEnd && (
-  //         <Text>Game in Progress...</Text>
-  //       )}
-  //       {game.state >= GameState.GameEnd && <Text>Game Ended</Text>}
-  //     </CardFooter>
-  //   </Card>
-  // );
+    getGame();
+    return () => {
+      setState = false;
+    };
+  }, [wagmiConfig, contractCfg, setGame, gameId]);
+
+  if (!game || !userAccount) {
+    return <></>;
+  }
+
+  const userJoinedGame = game.players.includes(userAccount);
+
+  return (
+    <Card w={500}>
+      <LinkBox>
+        <CardHeader>
+          <LinkOverlay as={NextLink} href={`/game/${gameId}`}>
+            <strong>Game ID: {gameId}</strong>
+          </LinkOverlay>
+        </CardHeader>
+
+        <CardBody>
+          <Text>Players: {game.players.length}</Text>
+          <UnorderedList styleType="- ">
+            {game.players.map((p) => (
+              <ListItem key={`game-${gameId}-${p}`} fontSize={14}>
+                {p}
+              </ListItem>
+            ))}
+          </UnorderedList>
+          <Text>
+            State: <strong>{GameState[game.state]}</strong>
+          </Text>
+          <Text>Created: {formatter.dateTime(game.startTime)}</Text>
+          <Text>Last Updated: {formatter.dateTime(game.lastUpdate)}</Text>
+        </CardBody>
+      </LinkBox>
+      <CardFooter justifyContent="center">
+        {game.state === GameState.GameInitiated && (
+          <Button
+            onClick={joinGameHandler}
+            variant="outline"
+            colorScheme="blue"
+            isDisabled={userJoinedGame}
+            isLoading={isPending}
+          >
+            {userJoinedGame ? "Already Joined" : "Join Game"}
+          </Button>
+        )}
+        {game.state >= GameState.RoundCommit && game.state <= GameState.RoundEnd && (
+          <Text>Game in Progress...</Text>
+        )}
+        {game.state >= GameState.GameEnd && <Text>Game Ended</Text>}
+      </CardFooter>
+    </Card>
+  );
 }
